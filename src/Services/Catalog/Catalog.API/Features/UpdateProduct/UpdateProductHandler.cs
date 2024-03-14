1e5deb54812f36a10269bc53eeed8222af481990
Tayfun Yirdem <tayfunyirdem@gmail.com>
2024-03-14 16:00:48 +0300

    UpdateProductCommandValidator is implemented

--- src/Services/Catalog/Catalog.API/Features/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Features/UpdateProduct/UpdateProductHandler.cs
@@ -10,6 +10,16 @@ public record UpdateProductCommand(
     string ImageFile,
     decimal Price) : ICommand<UpdateProductResult>;
 
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required").Length(2, 150).WithMessage("Name must be between 2 and 150 chars");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+
 internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
     public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
