d6fa40cb120ba960ae7a9a24b23ed8d59be611a6
Tayfun Yirdem <tayfunyirdem@gmail.com>
2024-03-17 13:57:36 +0300

    BasketCheckout integration event is implemented

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -3,6 +3,7 @@ using Common.Abstractions;
 using Common.Messaging.MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application;
 
@@ -17,6 +18,8 @@ public static class DependencyInjection
             config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
+        services.AddFeatureManagement();
+        
         services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
         
         return services;
